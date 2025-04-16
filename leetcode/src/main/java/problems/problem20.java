019d5ed16e2a7909f8f3919ced8c595c61f9f331
Loc203 <tranlocmom@gmail.com>
2025-04-16 20:49:38 +0700

    i hate leetcode

---
+++ leetcode/src/main/java/problems/problem20.java
@@ -0,0 +1,64 @@
+package problems;
+
+import java.util.Stack;
+
+/**
+ * Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+ * <p>
+ * An input string is valid if:
+ * <p>
+ * Open brackets must be closed by the same type of brackets.
+ * Open brackets must be closed in the correct order.
+ * Every close bracket has a corresponding open bracket of the same type.
+ * <p>
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: s = "()"
+ * <p>
+ * Output: true
+ * <p>
+ * Example 2:
+ * <p>
+ * Input: s = "()[]{}"
+ * <p>
+ * Output: true
+ * <p>
+ * Example 3:
+ * <p>
+ * Input: s = "(]"
+ * <p>
+ * Output: false
+ * <p>
+ * Example 4:
+ * <p>
+ * Input: s = "([])"
+ * <p>
+ * Output: true
+ */
+public class problem20 {
+    public static boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+
+            if (ch == '(' || ch == '{' || ch == '[') {
+                stack.push(ch);
+            } else if (!stack.empty() && ch == ')' && stack.peek() == '(') {
+                stack.pop();
+            } else if (!stack.empty() && ch == '}' && stack.peek() == '{') {
+                stack.pop();
+            } else if (!stack.empty() && ch == ']' && stack.peek() == '[') {
+                stack.pop();
+            } else {
+                return false;
+            }
+        }
+        return stack.isEmpty();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isValid("(){[{}]}"));
+    }
+}
---
+++ leetcode/src/main/java/problems/problem35.java
@@ -0,0 +1,56 @@
+package problems;
+
+/**
+ * Given a sorted array of distinct integers and a target value, return the index if the target is found. If not, return the index where it would be if it were inserted in order.
+ * <p>
+ * You must write an algorithm with O(log n) runtime complexity.
+ * <p>
+ * <p>
+ * <p>
+ * Example 1:
+ * <p>
+ * Input: nums = [1,3,5,6], target = 5
+ * Output: 2
+ * Example 2:
+ * <p>
+ * Input: nums = [1,3,5,6], target = 2
+ * Output: 1
+ * Example 3:
+ * <p>
+ * Input: nums = [1,3,5,6], target = 7
+ * Output: 4
+ * <p>
+ * <p>
+ * Constraints:
+ * <p>
+ * 1 <= nums.length <= 104
+ * -104 <= nums[i] <= 104
+ * nums contains distinct values sorted in ascending order.
+ * -104 <= target <= 104
+ */
+public class problem35 {
+    public static int searchInsert(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+        int mid;
+
+        while (left <= right) {
+            mid = left + (right - left) / 2;
+
+            if (target == nums[mid]) {
+                return mid;
+            } else if (target > nums[mid]) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return left;
+    }
+
+    public static void main(String[] args) {
+        int[] a = {1,3,5,6,8,9,13};
+        System.out.println(searchInsert(a,4 ));
+    }
+}
+
